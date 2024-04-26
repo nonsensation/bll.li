b14a6491bd593470bcaba9fbeaae5f501f3b4b5a
3li <nonsensation@gmail.com>
2024-04-26 14:40:13 +0200

    fix header 1

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,30 @@
 	import '../markdown.css';
 	import Navigation from '$lib/components/Navigation.svelte';
 	import BreadNav from '$lib/components/BreadNav.svelte';
+	import { NavBrand, NavHamburger, NavLi, NavUl, Navbar } from 'flowbite-svelte';
 </script>
 
 <div class="layout flex flex-col items-stretch gap-4 w-full h-full min-h-screen bg-red-white dark:bg-black text-black dark:text-white">
 	<header class="flex">
-		<Navigation />
+		<!-- <Navigation /> -->
+
+		<Navbar  >
+			<NavBrand href="/">
+			  <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
+			  <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+			</NavBrand>
+			<NavHamburger  />
+			<NavUl >
+			  <NavLi href="/">Home</NavLi>
+			  <NavLi href="/about">About</NavLi>
+			  <NavLi href="/docs/components/navbar">Navbar</NavLi>
+			  <NavLi href="/pricing">Pricing</NavLi>
+			  <NavLi href="/contact">Contact</NavLi>
+			</NavUl>
+		  </Navbar>
+		  
+		  
+		  
 	</header>
 	<main class="flex flex-col flex-grow w-full justify-start items-center gap-4 px-[5%]">
 		<div class="page-header self-start">
